5eb344940ad267444ea47918f739c3de225a9f07
Denatkins <dennisodibo@yahoo.com>
2022-06-30 17:17:59 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  * Return: Always 0 (success)
  */
@@ -8,4 +8,6 @@ int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+{
+Code goes here	
 }
