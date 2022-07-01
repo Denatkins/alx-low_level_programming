2127b87c9cf349eae2904b4bb2e46584b6682059
Denatkins <dennisodibo@yahoo.com>
2022-06-30 18:26:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
-main.c int main(int argc, char *argv[]) {
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
