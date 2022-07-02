2883443e268e06b5d957d041e4a78a85c382efcf
Denatkins <dennisodibo@yahoo.com>
2022-07-02 13:21:55 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - Prints all combinations of two digits with,
-* and space followed by new line
+*and space followed by new line
 *Return: returns 0
 */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 int digit1, digit2;
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)   
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
