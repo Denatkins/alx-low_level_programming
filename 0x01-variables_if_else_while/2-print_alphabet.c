73d7d552a1f5a2438e1f002f12fea74d75a18ed0
Denatkins <dennisodibo@yahoo.com>
2022-07-02 08:21:35 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
-* main - Entry point
-* Return: Always 0 (Success)
+*main - Program to print alphabet letter followed by new line
+*Return: return 0
 */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if ((n % 10) > 5)
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+letter = tolower(letter);
+putchar(letter);
 }
-else if ((n % 10) < 6 && (n % 10) != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-}
-else
-{
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
-}
+putchar('\n');
 return (0);
 }
