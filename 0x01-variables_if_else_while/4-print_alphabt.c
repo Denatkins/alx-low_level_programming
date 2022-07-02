b726b97b2b2fa37c51ad0cebc0780d794918ddbb
Denatkins <dennisodibo@yahoo.com>
2022-07-02 09:49:08 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
 *main - Entry poimt
 *Return: Always 0
@@ -7,8 +8,16 @@ int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-for (letter = 'A'; letter <= 'Z'; letter++)
+{
 putchar (letter);
+{
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar (letter);
+}
+break;
+}
+}
 putchar ('\n');
 return (0);
 }
