403a2daa5e466f6f247aa213211627a1fa88c60e
Denatkins <dennisodibo@yahoo.com>
2022-07-12 06:52:48 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,7 +19,7 @@ for (x = 0; x < 100; x++)
 pass[x] = rand() % 78;
 sum += (pass[x] + '0');
 putchar(pass[x] + '0');
-if ((2772 - sum) - '0' < 78)
+if ((2772 - sum) -'0' < 78)
 {
 y = 2772 - sum - '0';
 sum += y;
