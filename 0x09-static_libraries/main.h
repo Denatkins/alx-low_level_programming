cbea1833b73174a24e7acb39e0f7adaab98fc073
Denatkins <dennisodibo@yahoo.com>
2022-07-24 14:14:40 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
+void _puts(char *str);
 int puts(const char *string);
 int _isupper(int c);
 int _isdigit(int c);
