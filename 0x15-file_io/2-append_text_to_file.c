797dab3b688883982733ee348b8dffc1e132d6c5
Denatkins <dennisodibo@yahoo.com>
2022-08-22 10:16:45 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,12 +33,12 @@ ssize_t len;
 if (filename == NULL)
 return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
-f (fd == -1)
+if (fd == -1)
 return (-1);
 if (text_content != NULL)
 len = write(fd, text_content, _strlen(text_content));
 close(fd);
-f (len == -1)
+if (len == -1)
 return (-1);
 return (1);
 }
